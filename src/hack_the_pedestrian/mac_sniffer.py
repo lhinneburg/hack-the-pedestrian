ce4a70a07822cc8dc67bba366493f6e5cdb0dbdb
tbendziula <timo.bendziula@parimeo.de>
2023-06-17 15:26:23 +0200

    Fix async op

--- src/hack_the_pedestrian/mac_sniffer.py
+++ src/hack_the_pedestrian/mac_sniffer.py
@@ -11,7 +11,7 @@ async def scan_for_devices():
     async with airmon(interfaces[0]) as mon:
         async with pyrcrack.AirodumpNg() as pdump:
             async for result in pdump(mon.monitor_interface):
-                print(result.table)
+                await print(result)
                 await asyncio.sleep(2)
 
 
